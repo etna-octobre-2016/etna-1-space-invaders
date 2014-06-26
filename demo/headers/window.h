d9ea7fb04b8f3e71789709c2d1927a1c72e6d058
Said AHEMT <said.ahemt@nolaroads.com>
2014-06-26 19:47:43 +0200

    Affiche la fenêtre en mode plein écran

--- demo/headers/window.h
+++ demo/headers/window.h
@@ -4,12 +4,12 @@
   #include  <stdbool.h>
   #include  <SDL2.framework/Headers/SDL.h>
 
-  #define   WINDOW_TITLE    "Ma fenetre de ouf"
-  #define   WINDOW_HEIGHT   400
-  #define   WINDOW_WIDTH    400
+  #define   WINDOW_TITLE    ""
+  #define   WINDOW_HEIGHT   0
+  #define   WINDOW_WIDTH    0
   #define   WINDOW_POS_X    SDL_WINDOWPOS_UNDEFINED
   #define   WINDOW_POS_Y    SDL_WINDOWPOS_UNDEFINED
-  #define   WINDOW_FLAGS    SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
+  #define   WINDOW_FLAGS    SDL_WINDOW_FULLSCREEN_DESKTOP | SDL_WINDOW_INPUT_GRABBED
 
   void      window_destroy(SDL_Window *);
   bool      window_init(SDL_Window *);
