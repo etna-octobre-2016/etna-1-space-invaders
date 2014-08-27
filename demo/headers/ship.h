903cdff8fe75457272456dde4e84da0d9938b7a0
Edwige BERTEAUX <bertea_e@etna-alternance.net>
2014-08-27 15:12:23 +0200

    optimisation du code gestion des collisions dans game_loop

--- demo/headers/ship.h
+++ demo/headers/ship.h
@@ -3,7 +3,7 @@
   bool  ship_init();
   void  ship_clear(t_SDL_objects *);
   void  ship_move(int direction, t_SDL_objects *);
-  void  ship_draw(t_SDL_objects *);
+  void  ship_draw(t_SDL_objects *SDL, int previous_state);
   bool  ship_is_crash(t_SDL_objects *SDL);
   void  ship_update_life(t_SDL_objects *SDL, int number);
   bool  ship_is_in_life(t_SDL_objects *SDL);
