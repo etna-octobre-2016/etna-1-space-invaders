3757ec35334ed9db3eb3a1e5e146e7ad91aa19a5
Edwige BERTEAUX <bertea_e@etna-alternance.net>
2014-09-03 11:03:41 +0200

    correction du nom de la fonction

--- demo/headers/ship.h
+++ demo/headers/ship.h
@@ -4,7 +4,7 @@
   void  ship_clear(t_SDL_objects *);
   void  ship_move(int direction, t_SDL_objects *);
   void  ship_draw(t_SDL_objects *SDL);
-  bool  ship_is_crash(t_SDL_objects *SDL);
+  bool  ship_is_crashed(t_SDL_objects *SDL);
   void  ship_update_life(t_SDL_objects *SDL, int number);
   bool  ship_is_in_life(t_SDL_objects *SDL);
   void  ship_update_image(t_SDL_objects *SDL);
