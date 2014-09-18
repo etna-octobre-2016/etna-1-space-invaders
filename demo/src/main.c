444c76a5ca621af55ca4cca16b54159ea951b877
Said AHEMT <said.ahemt@nolaroads.com>
2014-09-18 16:20:26 +0200

    retire les printf de debug dans le main.c

--- demo/src/main.c
+++ demo/src/main.c
@@ -7,11 +7,8 @@ int                 main()
   srand(time(NULL));
   if (init(&SDL) == true)
   {
-    printf("Initialisation principale : ok\n");
     listen_events(&SDL);
-    printf("Traitement des évênements : ok\n");
     clear(&SDL);
-    printf("clear géneral: ok\n");
     exit(EXIT_SUCCESS);
   }
   else
@@ -25,25 +22,18 @@ bool                init(t_SDL_objects *SDL)
 {
   if (!(SDL_Init(SDL_INIT_VIDEO) < 0))
   {
-    printf("SDL_Init : ok\n");
     if (window_init(SDL) == true)
     {
-      printf("window_init : ok\n");
       if (renderer_init(SDL) == true)
       {
-        printf("renderer_init : ok\n");
         if (animation_init(SDL) == true)
         {
-          printf("animation_init : ok\n");
           if (ship_init(SDL) == true)
           {
-            printf("ship_init : ok\n");
             if (level_init(1, SDL) == true)
             {
-              printf("level_init : ok\n");
               if (enemy_init(SDL) == true)
               {
-                printf("enemy_init : ok\n");
                   return true;
               }
             }
@@ -63,23 +53,17 @@ bool                init(t_SDL_objects *SDL)
 void                clear(t_SDL_objects *SDL)
 {
   animation_clear(SDL);
-  printf("animation_clear : ok\n");
   ship_clear(SDL);
-  printf("ship_clear : ok\n");
   enemy_clear(SDL);
-  printf("enemy_clear : ok\n");
   if (SDL->renderer != NULL)
   {
     SDL_DestroyRenderer(SDL->renderer);
-    printf("SDL_renderer clear : ok\n");
   }
   if (SDL->window != NULL)
   {
     SDL_DestroyWindow(SDL->window);
-    printf("SDL_window clear : ok\n");
   }
   SDL_Quit();
-  printf("SDL_Quit clear : ok\n");
 }
 
 void                game_loop(t_SDL_objects *SDL)
@@ -126,11 +110,9 @@ void                listen_events(t_SDL_objects *SDL)
 
   while (opened)
   {
-    printf("Fenêtre ouverte !\n");
    /*SDL_PollEvent segfault...*/
     while (SDL_PollEvent(&event))
     {
-      printf("On attend un évênement !\n");
       if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE)
       {
         opened = false;
@@ -145,17 +127,14 @@ void                listen_events(t_SDL_objects *SDL)
       }
     }
 
-    printf("On traite le jeu\n");
     currentTime = SDL_GetTicks();
     timeDiff = (currentTime - previousTime);
     if (timeDiff > MAX_TIME_DIFF(FRAMES_PER_SECOND)) /* Code exécuté à la fréquence de FRAMES_PER_SECOND */
     {
       SDL_RenderClear(SDL->renderer);
       game_loop(SDL);
-      printf("Game Loop\n");
       if (!ship_is_in_life(SDL))
         opened = false;
-      printf("On a regardé si le vaisseau était en vie\n");
       SDL_RenderPresent(SDL->renderer);
       previousTime = currentTime;
     }
