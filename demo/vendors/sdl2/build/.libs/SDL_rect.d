d682f682003f5fdb7ec4ec7718c2c65441cc6137
Said AHEMT <said.ahemt@nolaroads.com>
2014-09-24 00:14:37 +0200

    compile le programme avec les libs compilées

---
+++ demo/vendors/sdl2/build/.libs/SDL_rect.d
@@ -0,0 +1,9 @@
+build/SDL_rect.lo: \
+  /Users/ahemt_s/Documents/etna/projets/spaceinvader/demo/vendors/sdl2/src/video/SDL_rect.c \
+  /Users/ahemt_s/Documents/etna/projets/spaceinvader/demo/vendors/sdl2/src/video/../SDL_internal.h \
+  /Users/ahemt_s/Documents/etna/projets/spaceinvader/demo/vendors/sdl2/src/video/../dynapi/SDL_dynapi.h \
+  /Users/ahemt_s/Documents/etna/projets/spaceinvader/demo/vendors/sdl2/src/video/../dynapi/SDL_dynapi_overrides.h \
+  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
+  include/close_code.h include/SDL_rect.h include/SDL_stdinc.h \
+  include/SDL_error.h include/SDL_pixels.h include/SDL_rwops.h \
+  /Users/ahemt_s/Documents/etna/projets/spaceinvader/demo/vendors/sdl2/src/video/SDL_rect_c.h
