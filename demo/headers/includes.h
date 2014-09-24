db3c4e49402a50812c02745773e602cfd6e50f1e
Said AHEMT <said.ahemt@nolaroads.com>
2014-09-24 21:44:33 +0200

    remet l'ancienne version du Makefile

--- demo/headers/includes.h
+++ demo/headers/includes.h
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
-#include "../libs/sdl2/include/SDL2/SDL.h"
-#include "../libs/sdl2-image/include/SDL2/SDL_image.h"
-#include "../libs/sdl2-ttf/include/SDL2/SDL_ttf.h"
+#include <SDL2/SDL.h>
+#include <SDL2_image.framework/Headers/SDL_image.h>
+#include <SDL2_ttf.framework/Headers/SDL_ttf.h>
 #include "uthash.h"
